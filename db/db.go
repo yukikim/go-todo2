76d9954af8a3524570ceec2b1867681752abe6a6
yukikim <tki6ra@gmail.com>
2026-07-02 14:42:54 +0900

    DB接続確認を削除

--- db/db.go
+++ db/db.go
@@ -6,32 +6,13 @@ import (
 	_ "github.com/lib/pq"
 )
 
+/*
+NewDB はデータベース接続を作成し、接続が成功した場合に *sql.DB を返します。
+接続に失敗した場合はエラーを返します。
+*/
 func NewDB() (*sql.DB, error) {
 	dsn := "host=localhost port=5432 user=postgres password=postgres dbname=go_todo2 sslmode=disable"
 
-	database, err := sql.Open("postgres", dsn)
-	if err != nil {
-		return nil, err
-	}
-
-	// データベース接続の確認
-	if err := database.Ping(); err != nil {
-		return nil, err
-	}
-
-	return database, nil
+	// sql.Open はデータベース接続を開きます。PostgreSQL ドライバを使用して、指定された DSN (Data Source Name) を使って接続します。
+	return sql.Open("postgres", dsn)
 }
-
-// Close はデータベース接続を閉じます。
-// func (db *sql.DB) Close() error {
-// 	return db.Close()
-// }
-
-// NewDB はデータベース接続を作成し、接続が成功した場合に *sql.DB を返します。
-// 接続に失敗した場合はエラーを返します。
-// func NewDB() (*sql.DB, error) {
-// 	dsn := "host=localhost port=5432 user=postgres password=postgres dbname=go_todo2 sslmode=disable"
-
-// 	// sql.Open はデータベース接続を開きます。PostgreSQL ドライバを使用して、指定された DSN (Data Source Name) を使って接続します。
-// 	return sql.Open("postgres", dsn)
-// }
